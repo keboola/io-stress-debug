42bdbc464b2f9fff26bda5ba161059f0371b1961
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2017-11-28 15:32:52 +0100

    more debug

--- direct-move.sh
+++ direct-move.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 export SLICES_COUNT=$1
 
 # mkdir $KBC_DATADIR/out/tables/slice.csv
@@ -12,11 +13,17 @@ cd $KBC_DATADIR/in/files
 
 echo "Dirs in $KBC_DATADIR/in/files"
 find . ! -iname "*.manifest" ! -name "." -type d | wc -l
-find . ! -iname "*.manifest" ! -name "." -type d | xargs -n1 -I {} mv {} $KBC_DATADIR/out/tables/{}.csv
+echo "Files in $KBC_DATADIR/in/files"
+ls $KBC_DATADIR/in/files | wc -l
+echo "Files in $KBC_DATADIR/in/files/slice"
+ls $KBC_DATADIR/in/files/slice | wc -l
+find . ! -iname "*.manifest" ! -name "." -type d | xargs -n1 -I {} mv -v {} $KBC_DATADIR/out/tables/{}.csv
 echo "Files in $KBC_DATADIR/in/files/slice"
 ls $KBC_DATADIR/in/files/slice | wc -l
 echo "Files in $KBC_DATADIR/out/tables/slice.csv"
 ls $KBC_DATADIR/out/tables/slice.csv | wc -l
-find . ! -iname "*.manifest" ! -name "." | xargs -n1 -I {} mv {} $KBC_DATADIR/out/tables/{}.csv
+echo "Files in $KBC_DATADIR/in/files"
+ls $KBC_DATADIR/in/files | wc -l
+find . ! -iname "*.manifest" ! -name "." | xargs -n1 -I {} mv -v {} $KBC_DATADIR/out/tables/{}.csv
 echo "Files in $KBC_DATADIR/out/tables"
 ls $KBC_DATADIR/out/tables | wc -l
