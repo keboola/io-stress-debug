c47f7dc9a5d1507f6974e39e0f8c28631246a037
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2017-11-28 11:35:55 +0100

    debug: quick find

--- direct-move.sh
+++ direct-move.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 export SLICES_COUNT=$1
 
-mkdir $KBC_DATADIR/out/tables/slice.csv
-for j in `seq 1 $SLICES_COUNT`;
-do
-    mv $KBC_DATADIR/in/files/slice/random$j $KBC_DATADIR/out/tables/slice.csv/
-done
+# mkdir $KBC_DATADIR/out/tables/slice.csv
+
+#for j in `seq 1 $SLICES_COUNT`;
+#do
+#    mv $KBC_DATADIR/in/files/slice/random$j $KBC_DATADIR/out/tables/slice.csv/
+#done
+
+cd $KBC_DATADIR/in/files
+
+find . ! -iname "*.manifest" ! -name "." -type d | xargs -n1 -I {} mv {} $KBC_DATADIR/out/tables/{}.csv
+
