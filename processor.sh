152207a124d172609b2cc8a2d81d2b38c340141a
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2017-11-28 11:13:41 +0100

    debug: remove shell wrap

--- processor.sh
+++ processor.sh
@@ -23,10 +23,10 @@ if [ $KBC_PARAMETER_DIRECTION == "tables" ]; then
     if [ $KBC_PARAMETER_ADDCSVSUFFIX == "true" ] || [ $KBC_PARAMETER_ADDCSVSUFFIX == "1" ]; then
         # move folders
         echo "Moving folders (with CSV suffix)"
-        find . ! -iname "*.manifest" ! -name "." -type d | xargs -n1 -I {} sh -c "mv \"{}\" $KBC_DATADIR/out/tables/\"{}\".csv"
+        find . ! -iname "*.manifest" ! -name "." -type d | xargs -n1 -I {} mv {} $KBC_DATADIR/out/tables/{}.csv
         # move files
         echo "Moving files (with CSV suffix)"
-        find . ! -iname "*.manifest" ! -name "." | xargs -n1 -I {} sh -c "mv \"{}\" $KBC_DATADIR/out/tables/\"{}\".csv"
+        find . ! -iname "*.manifest" ! -name "." | xargs -n1 -I {} mv {} $KBC_DATADIR/out/tables/{}.csv
         exit 0
     else
         # move folders
